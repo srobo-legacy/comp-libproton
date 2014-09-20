54bfe8de25d7048109ea4ac9d177ed8f3705a29f
Peter Law <PeterJCLaw@gmail.com>
2014-09-20 20:37:01 +0100

    Make the tests tolerant of other working directories
    When these tests are found by nose running tests against something
    which has consumed libproton as a submodule, it needs to not fall
    over becuase of path differences.
    
    Change-Id: I12885734b59ecbd6185719a567b3ee4284227aa0

--- test/helpers.py
+++ test/helpers.py
@@ -3,8 +3,12 @@ import os
 import sys
 import yaml
 
+def root():
+    mydir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.dirname(mydir)
+
 def path_bodge():
-    path = os.path.dirname(os.path.realpath(__file__)) + "/../libproton/"
+    path = os.path.join(root(), "libproton")
     sys.path.insert(0, path)
 
 def tla_result_fixture(zone_number, score = 0):
@@ -16,8 +20,8 @@ def tla_result_fixture(zone_number, score = 0):
     }
 
 def get_data(data_root, input_name):
-    input_file = os.path.join(data_root, input_name)
-    output_file = os.path.join(data_root, input_name[:-5] + '.out.yaml')
+    input_file = os.path.join(root(), data_root, input_name)
+    output_file = os.path.join(root(), data_root, input_name[:-5] + '.out.yaml')
 
     assert os.path.exists(output_file), "Missing output expectation '{1}' for input '{0}'.".format(input_name, output_file)
 
@@ -25,7 +29,7 @@ def get_data(data_root, input_name):
     return input_file, expected_output
 
 def get_input_files(data_root):
-    files = os.listdir(data_root)
+    files = os.listdir(os.path.join(root(), data_root))
     outputs = [f for f in files if f.endswith('.out.yaml')]
     inputs = [f for f in files if f.endswith('.yaml') and not f in outputs]
 
